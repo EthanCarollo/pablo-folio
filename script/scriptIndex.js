3312f8fdf0276e034082f038c970e7cfd8c0fa2b
Jacky <deku.private@gmail.com>
2023-01-28 13:45:44 +0100

    up

--- script/scriptIndex.js
+++ script/scriptIndex.js
@@ -1,7 +1,6 @@
 
 
 document.addEventListener("scroll", ()=>{
-    console.log(window.scrollY)
     if(window.scrollY > 0){
         document.querySelector("header").classList.add("minimize")
     }else{
